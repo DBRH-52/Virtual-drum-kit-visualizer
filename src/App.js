081042166db4c958997d9da7e25318a0e6e15004
DBRH-52 <dbrhx52@gmail.com>
2025-05-10 19:05:44 +0200

    refactor: styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { useEffect } from 'react';
-import { initializeMIDI } from './midi';
+import { initializeMIDI } from './midi/index';
 import './styles/index.css';
 
+// Import SVG files
+const kickSvg = "/images/kick.svg";
+const snareSvg = "/images/snare.svg";
+const hihatSvg = "/images/hihat.svg";
+const tomSvg = "/images/tom.svg";
+const crashSvg = "/images/crash.svg";
+const rideSvg = "/images/ride.svg";
+
+// Define drum types with their properties
 const DRUMS = [
-    'kick',
-    'snare',
-    'hihat',
-    'tom1',
-    'tom2',
-    'tom3',
-    'tom4',
-    'crash1',
-    'crash2',
-    'ride'
+    { id: 'kick', name: 'Kick', image: kickSvg },
+    { id: 'snare', name: 'Snare', image: snareSvg },
+    { id: 'hihat', name: 'Hihat', image: hihatSvg },
+    { id: 'tom1', name: 'Tom 1', image: tomSvg },
+    { id: 'tom2', name: 'Tom 2', image: tomSvg },
+    { id: 'tom3', name: 'Tom 3', image: tomSvg },
+    { id: 'tom4', name: 'Tom 4', image: tomSvg },
+    { id: 'crash1', name: 'Crash 1', image: crashSvg },
+    { id: 'crash2', name: 'Crash 2', image: crashSvg },
+    { id: 'ride', name: 'Ride', image: rideSvg }
 ];
 
 function App() {
@@ -26,11 +35,9 @@ function App() {
         <div className="App">
             <h1>Drum Visualizer</h1>
             <div id="drum-container">
-                {/* Iterate over the DRUMS array */}
                 {DRUMS.map((drum) => (
-                    <div key={drum} className="drum" id={drum}>
-                        {/* Display the drum name in more readable format */}
-                        {drum.replace(/([a-z])([A-Z])/g, '$1 $2')}
+                    <div key={drum.id} className="drum" id={drum.id}>
+                        <img src={drum.image} alt={drum.name} className="drum-image" />
                     </div>
                 ))}
             </div>
